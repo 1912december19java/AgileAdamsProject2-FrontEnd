2c5cf00fd0ee6acba3dc9cb1ea7d52c31f8eba57
aishadeveloper <aisha.glasby@gmail.com>
2020-01-28 21:12:48 -0500

    created a login component with a dropdown and login function

--- project2/src/app/app.module.ts
+++ project2/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TagCloudModule } from 'angular-tag-cloud-module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
     
   ],
   imports: [
