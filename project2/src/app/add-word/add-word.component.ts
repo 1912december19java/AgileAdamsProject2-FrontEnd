79458700a2681535c64325aa7e97793ec8ecdbc6
aishadeveloper <aisha.glasby@gmail.com>
2020-02-01 11:00:25 -0500

    added add-word component

---
+++ project2/src/app/add-word/add-word.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-word',
+  templateUrl: './add-word.component.html',
+  styleUrls: ['./add-word.component.css']
+})
+export class AddWordComponent implements OnInit {
+
+  word = {
+    id: 1,
+    trainer_username: "dDuck",
+    user_username: 'mmouse',
+    word_text: "yourWord"
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  saveWord(): void{
+    alert("Your word is being sent to the DB");
+    //this.myServerice.saveWord(this.word);
+    this.resetWordForm();
+  }
+
+  resetWordForm(){
+    this.word.trainer_username = "";
+    this.word.user_username = "";
+    this.word.word_text= "";
+  }
+
+}
